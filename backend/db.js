54140d5db163be80b949aa7510421c1ecb152a73
Anne <a.morillon101298@gmail.com>
2026-06-01 22:12:50 +0200

    fix: change variable name in DB_HOST

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
